cff061316e1b08429005e5e9965c8889cddfdb5c
thanhcong051593 <42972932+thanhcong051593@users.noreply.github.com>
2018-09-05 07:30:32 +0700

    update

--- callback.js
+++ callback.js
@@ -13,9 +13,10 @@ function readFile(path,callback){
 }
 
 
-//callback function tính dien tich hinh tron với r được load từ data.txt
+//callback function tính diện tích hình tròn với dữ liệu được load từ file data.txt
+
 readFile('./data.txt', (data) =>{
-	var r = Number(data);
-	var s = Math.PI*r*r;
+	var r = Number(data); //parse string to number
+	var s = Math.PI*r*r; // công thức tính diện tích hình tròn
 	return s;
 });
